b3b1b98f8bb0b201fd0aa98167f503b61d607638
Samsa <motornijsemen@gmail.com>
2026-04-27 18:55:22 +0300

    2

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -18,6 +18,7 @@ export default function NoteDetailsClient() {
     queryKey: ["note", noteId],
     queryFn: () => fetchNoteById(noteId),
     enabled: Boolean(noteId),
+    refetchOnMount: false,
   });
 
   if (isLoading) {
